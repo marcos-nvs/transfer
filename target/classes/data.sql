e4f4ceff1089c8d4e2869aafca5db0d527d87aa7
marcos-nvs <naves11.mn@gmail.com>
2024-05-08 17:24:24 -0300

    Retirada de imports

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into customer (customerid, name) values (1, 'Client 1')
+insert into customer (customerid, name) values (2, 'Client 2')
+insert into customer (customerid, name) values (3, 'Client 3')
+
+insert into tax (taxid, datein, dateuntil, valuetax, taxpercent) values (1, 0, 0, 3, 2.5)
+insert into tax (taxid, datein, dateuntil, valuetax, taxpercent) values (2, 1, 10, 12, 0)
+insert into tax (taxid, datein, dateuntil, valuetax, taxpercent) values (3, 11, 20, 0, 8.2)
+insert into tax (taxid, datein, dateuntil, valuetax, taxpercent) values (4, 21, 30, 0, 6.9)
+insert into tax (taxid, datein, dateuntil, valuetax, taxpercent) values (5, 31, 40, 0, 4.7)
+insert into tax (taxid, datein, dateuntil, valuetax, taxpercent) values (6, 41, 50, 0, 1.7)
+
